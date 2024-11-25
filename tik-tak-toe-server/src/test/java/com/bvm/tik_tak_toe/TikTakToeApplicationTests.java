d8afe00a2f56979efb791f6d14b7c2164a257cbd
Bavlly magid <pavllymagid04@gmail.com>
2024-11-25 22:09:46 +0200

    [ADDED] server code

---
+++ tik-tak-toe-server/src/test/java/com/bvm/tik_tak_toe/TikTakToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvm.tik_tak_toe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikTakToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
