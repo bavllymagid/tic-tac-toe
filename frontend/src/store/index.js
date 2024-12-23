53be4ed5e635bc229ffa5bfbc38f2d5845a0ec7c
Bavlly magid <pavllymagid04@gmail.com>
2024-12-24 00:05:56 +0200

    [FIXED] grid state

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,21 +1,8 @@
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import gameReducer from './gameSlice';
-
-const persistConfig = {
-  key: 'game',
-  storage,
-  whitelist: ['mode', 'grid', 'superGrid'], // Specify which state to persist
-};
-
-const persistedReducer = persistReducer(persistConfig, gameReducer);
 
 export const store = configureStore({
   reducer: {
-    game: persistedReducer,
+    game: gameReducer,
   },
 });
-
-export const persistor = persistStore(store);
